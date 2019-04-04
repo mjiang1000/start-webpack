e052c4a9429e8082af403e95a6f4d1152d77e384
姜闽 <jiangm@mingyuanyun.com>
2019-04-04 19:33:43 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,48 @@
-const webpackHtmlPlugin = require("webpack-html-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
+const webpack = require("webpack")
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     mode: "development",
-    devtool: "source-map",
+    devtool: "#source-map",
     entry: {
         app: "./index.js"
     },
-    // output: "./dist",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        publicPath: "./"
+    },
+    resolve:{extensions: [".js", ".jsx", ".ts", ".tsx"]},
     module: {
         rules: [
             {test: /\.jsx?$/, loader: 'babel-loader'},
             {test: /\.tsx?$/, loader: 'ts-loader'},
         ],
-        noParse: /node_modules/
     },
     plugins: [
-        new webpackHtmlPlugin({
-            template: path.resolve(__dirname, "./index.html")
-        })
+        new HtmlWebpackPlugin({
+			template: 'index.html'
+        }),
+        // new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': JSON.stringify('"development'),
+        // }),
+        // new CleanWebpackPlugin(['dist']),
+        new webpack.HotModuleReplacementPlugin(),
     ],
     devServer: {
-        port: 8081
-    }
+        port: 8081,
+        contentBase: path.resolve(__dirname, "./dist"),
+        // open: true,
+        historyApiFallback: {
+            rewrites: [
+                {from: /app\.js$/, to: "/app.js"},
+                {from : "/*", to: "/index.html"}
+            ]
+        }
+    },
+    watchOptions: {
+		ignored: /node_modules/
+	}
 }
