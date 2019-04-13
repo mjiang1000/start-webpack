0bb8574122024d45cbd947af267b2d29f5966c58
mjiang1000@gmail.com <mjiang1000@gmail.com>
2019-04-13 17:13:28 +0800

    add comment

--- src/modules/reduxForm/basicForm/index.jsx
+++ src/modules/reduxForm/basicForm/index.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import { combineReducers, createStore } from "redux"
 import { connect, Provider } from "react-redux"
-import { reducer as fromReducer } from "redux-form"
+import { reducer as formReducer } from "redux-form"
 
 import Contact from "./ContactFrom"
 import Todo from "../../redux/todoList/containers/app"
 import todos from "../../redux/todoList/reducers/index"
+
 const rootReducer = combineReducers({
     todos: todos,
-    form: fromReducer
+    form: formReducer,  // 默认key form在rootReducer下
 })
 
 const store = createStore(rootReducer)
@@ -21,5 +22,6 @@ class BasicFrom extends React.Component {
         return <Contact onSubmit={this.handleSubmit}/>
     }
 }
-const app = () => <Provider store={store}><BasicFrom /> <Todo /></Provider>
+
+const app = () => <Provider store={store}><BasicFrom /><Todo /></Provider>
 export default app
